e2eaf12d468a756e622457c5ca04ebd497e3d78c
Joshua Barrass <josh.barrass.work@gmail.com>
2024-09-24 18:57:02 +0100

    Get the images as column vectors and write the mean face

--- problem2.cpp
+++ problem2.cpp
@@ -19,6 +19,12 @@ constexpr int SUBIM_WIDTH = 128;
 constexpr int SUBIM_HEIGHT = 155;
 constexpr int SUBIM_PIXELS = SUBIM_WIDTH * SUBIM_HEIGHT;
 
+void write_image(std::filesystem::path out_file, gsl_vector *flat, int width, int height) {
+  // convert to a matrix via a view
+  gsl_matrix_view view = gsl_matrix_view_vector(flat, height, width);
+  save_matrix_to_image(&view.matrix, out_file);
+}
+
 int main() {
   gsl_matrix *im_matrix_raw;
   if (!load_channel_to_matrix(im_matrix_raw, "Faces.png", 0)) {
@@ -32,8 +38,10 @@ int main() {
   gsl_matrix_view im_matrix_view = gsl_matrix_submatrix(im_matrix_raw, 1, 0, SUBIM_HEIGHT*SUBIM_ROWS, SUBIM_WIDTH*SUBIM_COLS);
   gsl_matrix *im_matrix = &im_matrix_view.matrix;
 
-  // reformat the data into a bunch of row vectors, one for each image
-  gsl_matrix *X = gsl_matrix_alloc(SUBIMS, SUBIM_PIXELS);
+  // reformat the data into a bunch of column vectors, one for each image
+  // we have to use column vectors to extract the correct vectors
+  // otherwise, the SVD function will not be compatible with the data
+  gsl_matrix *X = gsl_matrix_alloc(SUBIM_PIXELS, SUBIMS);
   for (int y = 0; y < SUBIM_ROWS; ++y) {
     for (int x = 0; x < SUBIM_COLS; ++x) {
       const int subim = y*SUBIM_COLS + x;
@@ -44,7 +52,7 @@ int main() {
       for (int i = 0; i < SUBIM_HEIGHT; ++i) {
         for (int j = 0; j < SUBIM_WIDTH; ++j) {
           const int pixel = SUBIM_WIDTH*i + j;
-          gsl_matrix_set(X, subim, pixel, gsl_matrix_get(&im_view.matrix, i, j));
+          gsl_matrix_set(X, pixel, subim, gsl_matrix_get(&im_view.matrix, i, j));
         }
       }
     }
@@ -52,4 +60,17 @@ int main() {
 
   // dealloc the original matrix now, since it's redundant
   gsl_matrix_free(im_matrix_raw);
+
+  // do the principle component analysis
+  // we will use the components later, but the centre also tells us
+  // the mean vector!
+  gsl_vector *centre, *eigenvals;
+  gsl_matrix *eigenvecs = do_pca(X, ColumnVector, centre, eigenvals);
+
+  std::cout << "Got " << eigenvecs->size2 << " eigenvecs of length " << eigenvecs->size1 << std::endl;
+  std::cout << "Centre length: " << centre->size << std::endl;
+  std::cout << "Centre max: " << gsl_vector_max(centre) << std::endl;
+  std::cout << "Centre min: " << gsl_vector_min(centre) << std::endl;
+  // save the mean vector as an image
+  write_image("x0.png", centre, SUBIM_WIDTH, SUBIM_HEIGHT);
 }
