b077751713a215fdceb2deccb54dfe10e57fc884
Joshua Barrass <josh.barrass.work@gmail.com>
2024-09-26 15:07:10 +0100

    Constify

--- include/immatrix.h
+++ include/immatrix.h
@@ -4,7 +4,7 @@
 #include "gsl/gsl_matrix.h"
 #include <filesystem>
 
-bool load_channel_to_matrix(gsl_matrix *&out, std::filesystem::path imfile, int channel);
-bool save_matrix_to_image(gsl_matrix *immatrix, std::filesystem::path imfile);
+bool load_channel_to_matrix(gsl_matrix *&out, const std::filesystem::path imfile, const int channel);
+bool save_matrix_to_image(const gsl_matrix *immatrix, const std::filesystem::path imfile);
 
 #endif 
